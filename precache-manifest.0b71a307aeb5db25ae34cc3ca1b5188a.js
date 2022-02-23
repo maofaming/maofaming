d9a3802d45a67248c908e6368abd0bc376bfd46a
maofaming <maofaming@baidu.com>
2022-02-23 16:59:07 +0800

    feat(view): 增加游戏页面

---
+++ precache-manifest.0b71a307aeb5db25ae34cc3ca1b5188a.js
@@ -0,0 +1,377 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1110923645f22f40668e",
+    "url": "/css/app.a4fa7180.css"
+  },
+  {
+    "revision": "016f3111ce85bae5a670",
+    "url": "/css/chunk-02b10ba1.1ea4eef1.css"
+  },
+  {
+    "revision": "2f59d37925b7fb78e075",
+    "url": "/css/chunk-2f4f19cb.0e433876.css"
+  },
+  {
+    "revision": "0edcee41c4634422a8e0",
+    "url": "/css/chunk-5d71c3ec.5dc69119.css"
+  },
+  {
+    "revision": "11772b73900553b2121f",
+    "url": "/css/chunk-5e8ee7f4.b0c0852a.css"
+  },
+  {
+    "revision": "4bfc6b00c6704c6a4db9",
+    "url": "/css/chunk-699ad5b0.0001bc60.css"
+  },
+  {
+    "revision": "2f5e2040eb05652dda6a",
+    "url": "/css/chunk-723619be.af4a2a58.css"
+  },
+  {
+    "revision": "b83a5627c4a6a9b0fca2",
+    "url": "/css/chunk-74e36537.b573cbc1.css"
+  },
+  {
+    "revision": "88805215a9b07a211f25",
+    "url": "/css/chunk-94605dde.37b59bb8.css"
+  },
+  {
+    "revision": "ece01629476ca96aad64",
+    "url": "/css/chunk-f907fd52.1ccde6c2.css"
+  },
+  {
+    "revision": "be288a82d921fb1f1be3",
+    "url": "/css/chunk-vendors.8cf56885.css"
+  },
+  {
+    "url": "/d220163a310a49eec32dcc2b19075d92.fnt"
+  },
+  {
+    "revision": "18d0895c22fb372bc596193fbaed280f",
+    "url": "/fonts/wenxue.18d0895c.ttf"
+  },
+  {
+    "revision": "368819ca9232c4514bb8f67a58bdd626",
+    "url": "/fonts/wenxue.368819ca.eot"
+  },
+  {
+    "revision": "c6e56486a133854334615e256db915b7",
+    "url": "/fonts/wenxue.c6e56486.woff"
+  },
+  {
+    "revision": "a32c4c1226d710054659a49e77b428a2",
+    "url": "/images/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "d47c5e99f7e104e6a89494ab15d5127f",
+    "url": "/images/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "f60fe434b3d651b4f1ef55d2c3dd4cf2",
+    "url": "/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "b88a6215d34ea65fbd054a95116a3906",
+    "url": "/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "e0435cc69c5b476b552d4372e0b006f8",
+    "url": "/images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "17da0f00dcdb4b756ff5ce68faf54940",
+    "url": "/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "f84e4202662d6c65b6d7459f64787a12",
+    "url": "/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "f84e4202662d6c65b6d7459f64787a12",
+    "url": "/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "4763ba89bfbe0ef4a81860e15747805d",
+    "url": "/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "116a342651394ab55394541d91f66ba3",
+    "url": "/images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "0fa2328d95318c8c1ae40359a0313b7e",
+    "url": "/img/2048@2x.0fa2328d.png"
+  },
+  {
+    "revision": "31c76c0e7f40548165fe0570f6b7cc91",
+    "url": "/img/atlas.31c76c0e.png"
+  },
+  {
+    "revision": "bbe30f77a17c380987898b1acff76ce7",
+    "url": "/img/back.bbe30f77.png"
+  },
+  {
+    "revision": "0591066822e3dc5a9fa192d6ff93febd",
+    "url": "/img/background.05910668.png"
+  },
+  {
+    "revision": "7ff68db5c7b29870636c9d2815052be4",
+    "url": "/img/background.7ff68db5.png"
+  },
+  {
+    "revision": "12a991f2481e53689bf1d46877260c75",
+    "url": "/img/banner.12a991f2.png"
+  },
+  {
+    "revision": "7210d9b9e31ba231695e4e2ed95cc87d",
+    "url": "/img/bg.7210d9b9.jpg"
+  },
+  {
+    "revision": "2bf0653aac429a1ec32d898d1b8cdd7c",
+    "url": "/img/building.2bf0653a.png"
+  },
+  {
+    "revision": "b87b79ce531c1b5bc7d9dd03520ba01e",
+    "url": "/img/buttons.b87b79ce.png"
+  },
+  {
+    "revision": "e489d0d8ab26d31704efb422cab06505",
+    "url": "/img/c4.e489d0d8.png"
+  },
+  {
+    "revision": "fd6730de6c4cdab9fbd193f09deaef15",
+    "url": "/img/c5.fd6730de.png"
+  },
+  {
+    "revision": "75f0935677ca855c6e6f43f53315de58",
+    "url": "/img/c8.75f09356.png"
+  },
+  {
+    "revision": "d24bd5bba0c8eb0a0180b1af22b4249b",
+    "url": "/img/f2.d24bd5bb.png"
+  },
+  {
+    "revision": "131969f76aac6258b7b5cecbce72b974",
+    "url": "/img/f3.131969f7.png"
+  },
+  {
+    "revision": "ae92207737dbf0a8227ec6215606b6b6",
+    "url": "/img/f7.ae922077.png"
+  },
+  {
+    "revision": "9b1a881e451f0ef93eede011517c59e8",
+    "url": "/img/flappybird@2x.9b1a881e.png"
+  },
+  {
+    "revision": "af0022d3f2f56fd94661560a954ff430",
+    "url": "/img/flappyfont.af0022d3.png"
+  },
+  {
+    "revision": "1071cf73930e1901f2272532c7288255",
+    "url": "/img/ground.1071cf73.png"
+  },
+  {
+    "revision": "5657e7613832dac5aa7f8d52fbe4f237",
+    "url": "/img/jump@2x.5657e761.png"
+  },
+  {
+    "revision": "4cf980a6909e949e55b27bd62dcb4fdf",
+    "url": "/img/jumpone@2x.4cf980a6.png"
+  },
+  {
+    "revision": "50dd06ce5cb34e6f99046c5a1d2bf600",
+    "url": "/img/main-index-logo.50dd06ce.png"
+  },
+  {
+    "revision": "f9b68a1cbb038d9577eea32332c236da",
+    "url": "/img/main-index-start.f9b68a1c.png"
+  },
+  {
+    "revision": "72cce4600fdfa1107c0a561e48af6154",
+    "url": "/img/main-index-title.72cce460.png"
+  },
+  {
+    "revision": "d33d37baaaa0f553c2244220158a846d",
+    "url": "/img/main-loading.d33d37ba.gif"
+  },
+  {
+    "revision": "2b111836c73ab22325944bd71c9c9199",
+    "url": "/img/main-modal-again-b.2b111836.png"
+  },
+  {
+    "revision": "2690111498b3d3fbfb66ada127a4b971",
+    "url": "/img/main-modal-bg.26901114.png"
+  },
+  {
+    "revision": "175bdb2b0afa6af2d579a0e4bd0e0b7c",
+    "url": "/img/main-modal-invite-b.175bdb2b.png"
+  },
+  {
+    "revision": "154a06aae802480cac0741536a82cdd4",
+    "url": "/img/main-modal-over.154a06aa.png"
+  },
+  {
+    "revision": "895ceb8ed81797129ff6372e8e413d1c",
+    "url": "/img/phaser.895ceb8e.png"
+  },
+  {
+    "revision": "056488f977cab6501abc4a6d81804001",
+    "url": "/img/pipes.056488f9.png"
+  },
+  {
+    "revision": "219af507455242af371699c7eea1d584",
+    "url": "/img/plates.219af507.png"
+  },
+  {
+    "revision": "57ca1a2085d82f0574e3ef740b9a5ead",
+    "url": "/img/preloader.57ca1a20.gif"
+  },
+  {
+    "revision": "e1759b8bf023b4014e9a524bb00f1255",
+    "url": "/img/score.e1759b8b.png"
+  },
+  {
+    "revision": "debf9f335570af88d85490260e5c70e1",
+    "url": "/img/tetris@2x.debf9f33.png"
+  },
+  {
+    "revision": "55590ee1eacc03edc17ce9183af3d0f5",
+    "url": "/img/wenxue.55590ee1.svg"
+  },
+  {
+    "revision": "8f001a4361e089b2b7b15d3ed4ba17d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1110923645f22f40668e",
+    "url": "/js/app.a3d08c0f.js"
+  },
+  {
+    "revision": "016f3111ce85bae5a670",
+    "url": "/js/chunk-02b10ba1.41aebaa9.js"
+  },
+  {
+    "revision": "2f59d37925b7fb78e075",
+    "url": "/js/chunk-2f4f19cb.ee04c605.js"
+  },
+  {
+    "revision": "0edcee41c4634422a8e0",
+    "url": "/js/chunk-5d71c3ec.42ef4355.js"
+  },
+  {
+    "revision": "11772b73900553b2121f",
+    "url": "/js/chunk-5e8ee7f4.442d8c4c.js"
+  },
+  {
+    "revision": "4bfc6b00c6704c6a4db9",
+    "url": "/js/chunk-699ad5b0.e179c0eb.js"
+  },
+  {
+    "revision": "2f5e2040eb05652dda6a",
+    "url": "/js/chunk-723619be.960ceaf6.js"
+  },
+  {
+    "revision": "b83a5627c4a6a9b0fca2",
+    "url": "/js/chunk-74e36537.36885962.js"
+  },
+  {
+    "revision": "f23cff2b214f8b7bdcca",
+    "url": "/js/chunk-7dac6c4f.3211b447.js"
+  },
+  {
+    "revision": "88805215a9b07a211f25",
+    "url": "/js/chunk-94605dde.bde6bf9d.js"
+  },
+  {
+    "revision": "ece01629476ca96aad64",
+    "url": "/js/chunk-f907fd52.23262935.js"
+  },
+  {
+    "revision": "be288a82d921fb1f1be3",
+    "url": "/js/chunk-vendors.031ce427.js"
+  },
+  {
+    "revision": "487d94c138d385f78c6da69c02ec4f9c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "9ed64417129e512b40cbd3b8066ba328",
+    "url": "/media/bg.9ed64417.mp3"
+  },
+  {
+    "revision": "3a9d380bc75819ea7d79c10078c35ead",
+    "url": "/media/bgm.3a9d380b.ogg"
+  },
+  {
+    "revision": "452631259961492dc242c9dc387029d4",
+    "url": "/media/bgm.45263125.mp3"
+  },
+  {
+    "revision": "6acf9a7179389374c1a364143eed031c",
+    "url": "/media/drop-perfect.6acf9a71.mp3"
+  },
+  {
+    "revision": "815a7e81d4ddc3d39f8f253015a68683",
+    "url": "/media/drop-perfect.815a7e81.ogg"
+  },
+  {
+    "revision": "78ee7bdda32a787a8221ff6f02d6654e",
+    "url": "/media/drop.78ee7bdd.ogg"
+  },
+  {
+    "revision": "c5015d0ccc8addcbd5d2de27fd224587",
+    "url": "/media/drop.c5015d0c.mp3"
+  },
+  {
+    "revision": "2b2246fd1222991cb8dfceedf06b642a",
+    "url": "/media/false1.2b2246fd.mp3"
+  },
+  {
+    "revision": "e2cd139054cbd5bc3ea9883a9ba11fc0",
+    "url": "/media/false2.e2cd1390.mp3"
+  },
+  {
+    "revision": "7be06c0e4ddae32e11ecfd151d14706c",
+    "url": "/media/flap.7be06c0e.wav"
+  },
+  {
+    "revision": "478d5b0961cb758d8b2af421ed11fd32",
+    "url": "/media/game-over.478d5b09.ogg"
+  },
+  {
+    "revision": "77e51a4dac0d34bc98a09c6b01fdc900",
+    "url": "/media/game-over.77e51a4d.mp3"
+  },
+  {
+    "revision": "75c4bc1869b797ad8eb8b122cce22dcc",
+    "url": "/media/music.75c4bc18.mp3"
+  },
+  {
+    "revision": "8183b3f6d3b8b09013eeaee277d29435",
+    "url": "/media/ouch.8183b3f6.wav"
+  },
+  {
+    "revision": "a5ab197768265e350f6244af4e373ea3",
+    "url": "/media/pipe-hit.a5ab1977.wav"
+  },
+  {
+    "revision": "01187a587a595112af8f0b4a9d790213",
+    "url": "/media/rotate.01187a58.ogg"
+  },
+  {
+    "revision": "146905b6969524624126383ea98838aa",
+    "url": "/media/rotate.146905b6.mp3"
+  },
+  {
+    "revision": "dd35f4f19ca89920bbd5eecb91ae56d6",
+    "url": "/media/score.dd35f4f1.wav"
+  },
+  {
+    "revision": "0e205959b542b869444196a05d0e17e0",
+    "url": "/media/spring.0e205959.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+
+importScripts(
+  "/precache-manifest.0b71a307aeb5db25ae34cc3ca1b5188a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "game-app"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
